b7e9266c3cff49aa3130f40317aabd1e3513986e
Martin-Stadler-CCA <martin.stadler@sz-ybbs.ac.at>
2021-08-02 18:53:27 +0200

    Connection to Database works fine, write values, read values and calculate changes too.

--- StoreValues.py
+++ StoreValues.py
@@ -3,7 +3,7 @@ import sys
 import requests
 from datetime import datetime
 
-#ssh -L 3306:localhost:3306 pi@cca1.hopto.org
+# ssh -L 3306:localhost:3306 pi@cca1.hopto.org
 
 day = datetime.now().strftime("%Y-%m-%d")
 hours = datetime.now().strftime("%H:%M:%S")
@@ -12,13 +12,6 @@ url = "https://api.nomics.com/v1/currencies/ticker?key=7447a1d2a3e1e93b18d9bbf00
 request = requests.get(url)
 data = request.json()
 request.close()
-
-#Calculating changes
-#
-#
-#
-#
-
 
 # Connect to MariaDB Platform
 try:
@@ -35,22 +28,33 @@ except mariadb.Error as e:
 
 # Get Cursor
 cur = conn.cursor()
+cur.execute("SELECT price, circulating_supply, market_cap, num_exchanges, volume FROM CRYPTO_DATA WHERE timestamp > "
+            "now() - interval 15 minute ORDER BY timestamp ASC LIMIT 1;")
+rows = cur.fetchone()
+
+price_past, circulating_supply_past, market_cap_past, num_exchanges, volume_past = rows
+
+# Calculating changes
+price_change_rel = str(price_past/float(data[0]["price"])*100)
+circulating_supply_change_rel = str(circulating_supply_past/int(data[0]["circulating_supply"])*100)
+market_cap_change_rel = str(market_cap_past/int(data[0]["market_cap"])*100)
+num_exchanges_change_rel = str(num_exchanges/int(data[0]["num_exchanges"])*100)
+volume_change_rel = str(volume_past/float(data[0]["1d"]["volume"])*100)
+
 cur.execute("INSERT INTO CRYPTO_DATA(id,name,price,circulating_supply,market_cap,num_exchanges,num_pairs,volume,"
             "price_change_rel,circulating_supply_change_rel,market_cap_change_rel,num_exchanges_change_rel,"
             "volume_change_rel,timestamp) values('" + data[0]["id"] + "','" + data[0]["name"] + "'," + data[0]["price"]
-            + "," + data[0]["circulating_supply"] + "," + data[0]["market_cap"] + "," + data[0]["num_exchanges"] + ","
-            + data[0]["num_pairs"] + "," + data[0]["1d"]["volume"] + ",0,0,0,0,0,CURRENT_TIMESTAMP)")
+            + "," + data[0]["circulating_supply"] + "," + data[0]["market_cap"] + "," + data[0]["num_exchanges"]
+            + "," + data[0]["num_pairs"] + "," + data[0]["1d"]["volume"] + "," + price_change_rel + "," +
+            circulating_supply_change_rel + "," + market_cap_change_rel + "," + num_exchanges_change_rel +
+            "," + volume_change_rel + ",CURRENT_TIMESTAMP)")
 
 # cur.execute("SELECT * FROM CRYPTO_DATA")
 conn.commit()
 
 cur.close()
 
-if(0):
-    print(data[0]["id"] + data[0]["name"] + data[0]["price"] + data[0]["circulating_supply"] + data[0]["market_cap"] + data[0]["num_exchanges"] +
-          data[0]["num_pairs"] + data[0]["1d"]["volume"] + data[0]["1d"]["price_change_rel"] + data[0]["circulating_supply_change_rel"] + data[0]["market_cap_change_rel"] +
-          data[0]["num_eschanges_change_rel"] + data[0]["volume_change_rel"])
+# price_change_rel = price - price 3min before
 
-#price_change_rel = price - price 3min before
 
 
