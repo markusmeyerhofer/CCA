6ec6cc10ec61fd9599874a8e2af759b25a136834
Martin-Stadler-CCA <martin.stadler@sz-ybbs.ac.at>
2021-08-25 12:03:06 +0200

    Connection to Database works fine, write values, read values and calculate changes too, Lunacrush-API is connected and writes values in all tables.

--- StoreValues.py
+++ StoreValues.py
@@ -38,11 +38,11 @@ row = cur.fetchone()
 price_past, circulating_supply_past, market_cap_past, num_exchanges, volume_past = row
 
 # Calculating changes
-price_change_rel = str(price_past/float(data[0]["price"])*100)
-circulating_supply_change_rel = str(circulating_supply_past/int(data[0]["circulating_supply"])*100)
-market_cap_change_rel = str(market_cap_past/int(data[0]["market_cap"])*100)
-num_exchanges_change_rel = str(num_exchanges/int(data[0]["num_exchanges"])*100)
-volume_change_rel = str(volume_past/float(data[0]["1d"]["volume"])*100)
+price_change_rel = str(price_past / float(data[0]["price"]) * 100)
+circulating_supply_change_rel = str(circulating_supply_past / int(data[0]["circulating_supply"]) * 100)
+market_cap_change_rel = str(market_cap_past / int(data[0]["market_cap"]) * 100)
+num_exchanges_change_rel = str(num_exchanges / int(data[0]["num_exchanges"]) * 100)
+volume_change_rel = str(volume_past / float(data[0]["1d"]["volume"]) * 100)
 
 # Insert into CRYPTO_DATA
 cur.execute("INSERT INTO CRYPTO_DATA(id,name,price,circulating_supply,market_cap,num_exchanges,num_pairs,volume,"
@@ -88,23 +88,23 @@ cur.execute("SELECT url_shares, unique_url_shares, reddit_posts, reddit_posts_sc
             " ASC LIMIT 1;")
 
 row = cur.fetchone()
-url_shares_past, unique_url_shares_past, reddit_posts_past, reddit_posts_score_past, reddit_comments_past,\
-reddit_comments_score_past, tweets_past, tweet_spam_past, tweet_retweets_past, galaxy_score_past,\
+url_shares_past, unique_url_shares_past, reddit_posts_past, reddit_posts_score_past, reddit_comments_past, \
+reddit_comments_score_past, tweets_past, tweet_spam_past, tweet_retweets_past, galaxy_score_past, \
 social_volume_global_past, social_contributors_past = row
 
 # Calculating changes
-url_shares_change_rel = url_shares_past/data2["url_shares"]*100
-unique_url_shares_change_rel = unique_url_shares_past/data2["unique_url_shares"]*100
-reddit_posts_change_rel = reddit_posts_past/data2["reddit_posts"]*100
-reddit_posts_score_change_rel = reddit_posts_score_past/data2["reddit_posts_score"]*100
-reddit_comments_change_rel = reddit_comments_past/data2["reddit_comments"]*100
-reddit_comments_score_change_rel = reddit_comments_score_past/data2["reddit_comments_score"]*100
-tweets_change_rel = tweets_past/data2["tweets"]*100
-tweet_spam_change_rel = tweet_spam_past/data2["tweet_spam"]*100
-tweet_retweets_change_rel = tweet_retweets_past/data2["tweet_retweets"]*100
-galaxy_score_change_rel = galaxy_score_past/data2["galaxy_score"]*100
-social_volume_global_change_rel = social_volume_global_past/data2["social_volume"]*100
-social_contributors_change_rel = social_contributors_past/data2["social_volume_global"]*100
+url_shares_change_rel = url_shares_past / data2["url_shares"] * 100
+unique_url_shares_change_rel = unique_url_shares_past / data2["unique_url_shares"] * 100
+reddit_posts_change_rel = reddit_posts_past / data2["reddit_posts"] * 100
+reddit_posts_score_change_rel = reddit_posts_score_past / data2["reddit_posts_score"] * 100
+reddit_comments_change_rel = reddit_comments_past / data2["reddit_comments"] * 100
+reddit_comments_score_change_rel = reddit_comments_score_past / data2["reddit_comments_score"] * 100
+tweets_change_rel = tweets_past / data2["tweets"] * 100
+tweet_spam_change_rel = tweet_spam_past / data2["tweet_spam"] * 100
+tweet_retweets_change_rel = tweet_retweets_past / data2["tweet_retweets"] * 100
+galaxy_score_change_rel = galaxy_score_past / data2["galaxy_score"] * 100
+social_volume_global_change_rel = social_volume_global_past / data2["social_volume"] * 100
+social_contributors_change_rel = social_contributors_past / data2["social_volume_global"] * 100
 
 # Insert into SOCIALMEDIA_DATA_RELATIVE
 cur.execute("INSERT INTO SOCIALMEDIA_DATA_RELATIV(id, url_shares_change_rel, unique_url_shares_change_rel,"
@@ -120,7 +120,6 @@ cur.execute("INSERT INTO SOCIALMEDIA_DATA_RELATIV(id, url_shares_change_rel, uni
             str(social_contributors_change_rel) + ",'" + str(timestamp) + "')")
 
 # Insert into SOCIALMEDIA_SENTIMENTS
-
 cur.execute("INSERT INTO SOCIALMEDIA_SENTIMENTS(id, tweet_sentiment1, tweet_sentiment2, tweet_sentiment3,"
             " tweet_sentiment4, tweet_sentiment5, tweet_sentiment_impact1, tweet_sentiment_impact2,"
             " tweet_sentiment_impact3, tweet_sentiment_impact4, tweet_sentiment_impact5, average_sentiment,"
@@ -135,4 +134,3 @@ cur.execute("INSERT INTO SOCIALMEDIA_SENTIMENTS(id, tweet_sentiment1, tweet_sent
 
 conn.commit()
 cur.close()
-
